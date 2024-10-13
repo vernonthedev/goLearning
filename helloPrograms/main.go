e1c4866673c41ea759e31ddd55a77063c948f5af
vernonthedev <techjaja2@gmail.com>
2024-10-13 05:47:46 -0700

    [+] Learnt how to add new packages

--- helloPrograms/main.go
+++ helloPrograms/main.go
@@ -1,11 +1,18 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "math"
+) 
 
 
 func main(){
     //this is how we initialize the function variable
-    name := "vernonthedev"
-	fmt.Println(name)
+    name, email := "vernonthedev" , "vernonthedev@gmail.com"
+	fmt.Println(name,email)
     fmt.Printf("%T\n",name)
+    //get the highest rounded value after the decimal
+    fmt.Println(math.Floor(2.7))
+    fmt.Println(math.Ceil(2.7))
+    fmt.Println(math.Sqrt(16))
 }
