da8c9286da3f4181564c908bf3ef9fb4d97e6eaf
vernonthedev <techjaja2@gmail.com>
2024-10-14 07:50:55 -0700

    [+] Learnt how to create struct value receivers.

--- structs/main.go
+++ structs/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "strconv"
+)
 
 //we create our first struct
 type Person struct{
@@ -17,5 +20,22 @@ func main(){
     //initialize the person struct
     firstPerson := Person{firstName: "Masamba", lastName: "Vernon", city: "California", gender: "Male", age: 20}
     fmt.Println(firstPerson)
+    secondPerson := Person{"vernon","thedev","California","Male",21}
+    fmt.Println(secondPerson)
+    fmt.Println("##############SPACE##############")
 
+    fmt.Println(secondPerson.firstName)
+    fmt.Println(secondPerson.firstName + secondPerson.lastName)
+
+
+    fmt.Println("##############SPACE##############")
+    fmt.Println(secondPerson.greet())
+}
+
+// Greeting method - value receiver
+func (person Person) greet() string {
+    return "Hello my name is " + 
+        person.firstName + " "+ 
+        person.lastName + " and I am " +    
+        strconv.Itoa(person.age)
 }
