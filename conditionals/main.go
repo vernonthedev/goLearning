295a9eead74416a0a07d3151598a65adea92938b
vernonthedev <techjaja2@gmail.com>
2024-10-16 06:06:13 -0700

    [+] Learnt how the maps always return a val and can be used.

--- conditionals/main.go
+++ conditionals/main.go
@@ -26,4 +26,21 @@ func main(){
 
     //get the integer range ids
     mappings.Rangeloop()
+
+
+    //more about maps
+    var myMap = map[string]uint8{"Adam":28, "Sarah":45}
+    fmt.Println(myMap["Adam"])
+    //return true if value exists in the map and false if it doesn't
+    var years, ok = myMap["Jason"]
+    if ok{
+        fmt.Printf("The age is %v", years)
+    }else{
+        fmt.Println("INVALID NAME")
+    }
+
+   // this 
+   // is some
+   // gibberish code
+   // am supposed to comment out
 }
