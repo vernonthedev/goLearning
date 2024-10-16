d7a12aa15278983a1d4a803cc7001535b10ad8b8
vernonthedev <techjaja2@gmail.com>
2024-10-16 05:56:58 -0700

    [+] Added and used append for slices.

--- arrays/main.go
+++ arrays/main.go
@@ -26,4 +26,10 @@ func main(){
     fmt.Println(len(slicedArray))
     fmt.Println(slicedArray)
 
+    //more about slices
+    var intSlice []int32 = []int32{4,5,6}
+    fmt.Println(intSlice)
+    intSlice = append(intSlice, 7)
+    fmt.Println(intSlice)
+
 }
